4360e20a3c68cfbb09c95862ca12b9543fd9ce9e
eduardo <eduardomendes418@gmail.com>
2026-06-18 10:18:29 -0300

    [35-FGS] - feat types

---
+++ types/product.ts
@@ -0,0 +1,21 @@
+export interface Product {
+  id: number;
+  name: string;
+  slug: string;
+  price: number;
+  unit: string;
+  image: string;
+  tag?: string;
+  description?: string;
+  gallery?: string[];
+  category?: string;
+  sku?: string;
+  tags?: string[];
+}
+
+export interface Category {
+  id: number;
+  name: string;
+  slug: string;
+  image: string;
+}
